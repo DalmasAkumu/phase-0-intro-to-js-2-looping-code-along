ca7c0b45e3d30bf99235e85a553df6458d5f42a8
Dalmas <akumuodhiambo@gmail.com>
2023-07-04 14:42:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);*/
+
+
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
